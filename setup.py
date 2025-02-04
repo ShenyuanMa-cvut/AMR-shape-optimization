704ccd467b9aa7e8171c09ceda65cd9f1a4c3236
Shenyuan Ma <shenyma@fel.cvut.cz>
2025-02-04 14:02:04 +0100

    set up package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='AMR_TO',
+      version='0.1',
+      description='An experimental implementation of the adaptive mesh refinement technique in topology optimization',
+      author='Shenyuan Ma',
+      author_email='shenyma@fel.cvut.cz',
+      license='MIT',
+    )
