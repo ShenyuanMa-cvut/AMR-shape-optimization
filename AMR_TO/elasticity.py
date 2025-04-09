df4bf7edb34d83cef163e333807f3ed9e22bcbd2
Shenyuan Ma <shenyma@fel.cvut.cz>
2025-04-09 19:30:14 +0200

    optimization loops okay save before implementing experiments parser

--- AMR_TO/elasticity.py
+++ AMR_TO/elasticity.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import ufl
 from basix.ufl import element
 import dolfinx
@@ -5,10 +7,9 @@ from dolfinx.fem import petsc
 
 from mpi4py import MPI
 import numpy as np
-import ufl.constant
 
-from .interpolation import local_quad_interpolation_cy,riesz_representation,_cell_areas
-from .microstructure import f_Ac_ufl
+from .interpolation import local_quad_interpolation_cy,riesz_representation,_cell_areas,averaging_kernel
+from .microstructure import solve_microstructure_batch
 
 def _epsilon(v : any ,dim : int):
     """
@@ -263,3 +264,167 @@ class ElasticitySolver(_AbstractElasticityForm):
         # Projection here
 
         return eta
+
+# Hooke's law
+def _Hooke(mu,lmbda,d):
+    """
+    Return the Hooke's law tensor in engineering notation for dimension d=2 or 3
+    """
+    if d==2:
+        return np.array([[2*mu+lmbda,lmbda,0],
+                         [lmbda,2*mu+lmbda,0],
+                         [0,0,2*mu]])
+    elif d==3:
+        return np.array([[2*mu+lmbda,lmbda,lmbda,0,0,0],
+                         [lmbda,2*mu+lmbda,lmbda,0,0,0],
+                         [lmbda,lmbda,2*mu+lmbda,0,0,0],
+                         [0,0,0,2*mu,0,0,],
+                         [0,0,0,0,2*mu,0],
+                         [0,0,0,0,0,2*mu]])
+
+import scipy.optimize as so
+def _fit_power_law(x,y):
+    def func(x,a,b,c):
+        return a+b*np.power(x, c)
+    
+    def jac(x,a,b,c):
+        da = np.ones_like(x)
+        db = np.power(x,c)
+        dc = np.power(x,c)*np.log(x)
+        return np.array([da,db,dc]).T
+
+    popt = so.curve_fit(func, np.array(x), np.array(y), [1.,1.,-1.], jac=jac)[0]
+    return popt
+
+def _marking(eta,threshold):
+    argsort = np.argsort(eta)[::-1]
+    size = argsort.size
+    return argsort[:int(size*threshold)]
+
+def _all_edges(mesh : dolfinx.mesh.Mesh,marked : np.ndarray):
+    dim = mesh.topology.dim
+    fdim = dim - 1
+    return np.unique(dolfinx.mesh.compute_incident_entities(mesh.topology, marked, dim, fdim))
+
+# @dataclass
+# class OptimizationParam():
+#     l : float
+#     delta : float
+#     inner_itermax : int
+#     ncell_max : int
+
+class Optimization():
+    """
+        Start the compliance minimization by microstructure loop.
+    """
+    def __init__(self, name : str, solver : ElasticitySolver, 
+                mesh0 : dolfinx.mesh.Mesh, 
+                mu, lmbda, 
+                is_adaptive : bool = False):
+        """
+            Initialize the optimization loops.
+            Solver : A fresh ElasticitySolver object
+            mesh0 : Initial Mesh
+            mu,lmbda : Lame's coefficients
+            is_adaptive : Whether the refinement is adaptive or not
+        """
+        self.name = name
+
+        self.solver = solver
+        self.mesh0 = mesh0
+        self.solver.update_mesh(self.mesh0)
+        self.Ave = averaging_kernel(self.solver.mesh)
+
+        self.mu = mu
+        self.lmbda = lmbda
+        self.A0 = _Hooke(self.mu, self.lmbda, self.solver.dim)
+        self.A0inv = np.linalg.inv(self.A0)
+
+        self.reset_design()
+
+        self.is_adaptive = is_adaptive
+
+    def reset_design(self):
+        A0 = self.A0
+        dim = self.solver.dim
+        edim = 6 if dim == 3 else 3
+        ncells = self.solver.ncells
+
+        A0h = np.zeros((ncells,edim,edim))
+        A0h[:] = A0[None,:,:]
+
+        self.solver.Ah.x.array[:] = A0h.reshape(-1)
+        self.solver.thetah.x.array[:] = 1.
+        compl,vol = self.solver.solve_all()
+
+    def main_loop(self,opt_param=None,post_action=None,final_action=None):
+        self.history = {'compl':[],'vol':[],'oc':[],'i':[]}
+        p = 1.05
+        i = 0
+
+        if opt_param is None:
+            opt_param = {'l':1.,
+                'delta':1e-3,
+                'inner_itermax':30,
+                'ncell_max':1000}
+
+        if type(opt_param) == dict:
+            l = opt_param.get('l',1.)
+            delta = opt_param.get('delta',1e-3)
+            inner_itermax = opt_param.get('inner_max',30)
+            ncell_max = opt_param.get('ncell_max', 1000)
+
+        if post_action is None:
+            post_action = lambda x,y:None
+        if final_action is None:
+            final_action = lambda x,y:None
+        
+        while self.solver.ncells <= ncell_max:
+            for k in range(inner_itermax):
+                #compute microstructures
+                tauhs = [tauh.x.array for tauh in self.solver.tauhs]
+                gtau, FA, y = solve_microstructure_batch(self.mu, self.lmbda, tauhs, self.solver.dim, self.solver.ncells)
+                FAinv = np.linalg.inv(FA)
+
+                #update design theta
+                self.solver.thetah.x.array[:] = self.Ave.dot(np.maximum(np.minimum(1.,np.sqrt(gtau/l)),delta))
+
+                #update Hooke's law
+                te = self.solver.thetah.x.array
+                self.solver.Ah.x.array[:] = np.linalg.inv(self.A0inv[None,:,:]+(1-te[:,None,None])/te[:,None,None]*FAinv).reshape(-1)
+
+                #update objective
+                compl,vol = self.solver.solve_all()
+                oc = self.solver.compute_oc(FAinv, l)
+
+                #update iteration
+                i += 1
+
+                self.history['compl'].append(sum(compl))
+                self.history['vol'].append(vol)
+                self.history['oc'].append(float(oc))
+                self.history['i'].append(i)
+                    
+                if k == 9:
+                    #fit a power law to oc
+                    k_ = k + 1
+                    a,b,c = _fit_power_law(range(2,k_+1),self.history['oc'][-k_+1:])
+                elif k > 10:
+                    if self.history['oc'][-1] <= a*p:
+                        break
+
+            if self.is_adaptive:
+                eta = self.solver.compute_indicator(FAinv, y, l, delta, self.mu, self.lmbda)
+                mark = _marking(eta, 0.1) #first 10% triangles with largest eta
+                edges = _all_edges(self.solver.mesh, mark) #take the union of all edges
+                new_mesh,_,_ = dolfinx.mesh.refine(self.solver.mesh, edges)
+            else:
+                new_mesh,_,_ = dolfinx.mesh.refine(self.solver.mesh)
+
+            post_action(self,opt_param)
+
+            self.solver.update_mesh(new_mesh)
+            self.Ave = averaging_kernel(self.solver.mesh)
+            compl,vol = self.solver.solve_all()
+        
+        final_action(self,opt_param)
--- demo/example0.py
+++ demo/example0.py
@@ -1,26 +1,14 @@
 from AMR_TO import elasticity as elas
-from AMR_TO.microstructure import solve_microstructure_batch
-from AMR_TO.interpolation import averaging_kernel
-
-import dolfinx,ufl
-import pyvista
-
-import pickle
-from matplotlib import pyplot as plt
-from mpi4py import MPI
 import numpy as np
 
 from utils import *
 
-def marking(eta,threshold):
-    argsort = np.argsort(eta)[::-1]
-    size = argsort.size
-    return argsort[:int(size*threshold)]
+def post_action(optimizer : elas.Optimization, opt_param):
+    return 
 
-def all_edges(mesh : dolfinx.mesh.Mesh,marked : np.ndarray):
-    dim = mesh.topology.dim
-    fdim = dim - 1
-    return np.unique(dolfinx.mesh.compute_incident_entities(mesh.topology, marked, dim, fdim))
+def final_action(optimizer : elas.Optimization, opt_param):
+    for (i,oc) in zip(optimizer.history['i'],optimizer.history['oc']):
+        print(f'iter {i} : {oc}')
 
 def main():
     # Define physical quantities
@@ -43,7 +31,7 @@ def main():
     load_points = [[(length/6,0.),(length/3,0.)],[(2*length/3,0.),(5*length/6,0.)]]
     load_points_raw = sum(load_points,start=[])
     lc = .05
-    new_mesh = generate_mesh(points,load_points_raw,lc=lc) #generate a very coarse mesh
+    mesh0 = generate_mesh(points,load_points_raw,lc=lc) #generate a very coarse mesh
 
     #marker of dirichlet bc
     def dirichlet(x : np.ndarray) -> np.ndarray[bool]:
@@ -67,81 +55,19 @@ def main():
 
     g = [np.array([0.,-0.1]),np.array([0.,-0.1])]
 
-    solver = elas.ElasticitySolver(dim, [dirichlet,dirichlet],[vn1,vn2],g) #intialize the abstract solver
-    #update the first mesh
-    solver.update_mesh(new_mesh)
-    Ave = averaging_kernel(new_mesh)
+    #initialize the abstract solver
+    solver = elas.ElasticitySolver(dim, [dirichlet,dirichlet],[vn1,vn2],g)
 
-    #initialize the design
-    ncells0 = new_mesh.topology.index_map(dim).size_local
-    A0h = np.zeros((ncells0,edim,edim))
-    A0h[:] = A0[None,:,:]
-    solver.Ah.x.array[:] = A0h.reshape(-1)
-    solver.thetah.x.array[:] = 1.
+    #initialize optimizer
+    optimizer = elas.Optimization("uniform_cantilever", solver, mesh0, mu, lmbda, False)
     
-    compl,vol = solver.solve_all() #initial solve
-
-    l = 5e-1
-    delta = 1e-4
-    itermax = 100
-
-    history = {'compl':[],'vol':[],'oc':[]}
-    i = 0
-    p = 1.05
-    ncells = ncells0
-
-    while ncells <= 24000:
-        solver.update_mesh(new_mesh)
-        solver.solve_all()
-        Ave = averaging_kernel(solver.mesh)
-        print(f"Refinement : new mesh has {solver.ncells} cells")
-        
-        print("k : comple vol oc")
-        for k in range(itermax):
-            
-            #compute microstructures
-            tauhs = [tauh.x.array for tauh in solver.tauhs]
-            gtau, FA, My = solve_microstructure_batch(mu, lmbda, tauhs, dim, solver.ncells)
-            FAinv = np.linalg.inv(FA)
-            
-            #update design theta
-            solver.thetah.x.array[:] = Ave.dot(np.maximum(np.minimum(1.,np.sqrt(gtau/l)),delta))
-            
-            #update Hooke's law
-            te = solver.thetah.x.array
-            solver.Ah.x.array[:] = np.linalg.inv(A0inv[None,:,:]+(1-te[:,None,None])/te[:,None,None]*FAinv).reshape(-1) 
-            
-            #update objective
-            compl,vol = solver.solve_all()
-            oc = solver.compute_oc(FAinv, l)
-            print(f"{k} : {sum(compl)} {vol} {oc}")
-
-            history['compl'].append(sum(compl))
-            history['vol'].append(vol)
-            history['oc'].append(oc)
-
-            if k == 9:
-                #fit a power law to oc
-                k_ = k + 1
-                a,b,c = fit_power_law(range(2,k_+1),history['oc'][-k_+1:])
-            elif k > 10:
-                if history['oc'][-1] <= a*p:
-                    break
-            
-        new_mesh,_,_ = dolfinx.mesh.refine(solver.mesh)
-        ncells = new_mesh.topology.index_map(dim).size_local
-        i += 1
-
-    #save the final mesh
-    topology_vtk, cell_types, x = dolfinx.plot.vtk_mesh(solver.mesh)
-    grid = pyvista.UnstructuredGrid(topology_vtk, cell_types, x)
-    grid.cell_data['theta'] = solver.thetah.x.array
-    grid.cell_data['h'] = solver.mesh.h(solver.dim, np.arange(solver.ncells))
-    grid.save(f"data/final_mesh_uni_{ncells0}_{solver.ncells}.vtu")
-
-    #save the obj history
-    with open(f"data/history_uni_{ncells0}_{solver.ncells}.pkl", "wb") as f:
-        pickle.dump(history, f)
+    #optimization parameters
+    opt_param = {'l':0.5,
+        'delta':1e-4,
+        'inner_itermax':30,
+        'ncell_max':10000}
+    
+    optimizer.main_loop(opt_param, post_action=post_action, final_action=final_action)
 
 if __name__ == '__main__':
     main()
